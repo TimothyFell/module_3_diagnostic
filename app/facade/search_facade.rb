f1ea127a98083fb24dd276d8147710fee90632ba
TimothyFell <timothy.fell846@gmail.com>
2018-12-12 10:00:37 -0700

    determined what parameters to send in the get request to get the desired response

--- app/facade/search_facade.rb
+++ app/facade/search_facade.rb
@@ -1,7 +1,11 @@
 class SearchFacade
 
   def stations
-    
+
+  end
+
+  def conn
+    Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=5N20sQn0jkCpVhdOEsoF3P43gvUKL5kx5F5Htuba&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10')
   end
 
 end
